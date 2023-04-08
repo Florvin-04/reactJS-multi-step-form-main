e75cb513ec201f5fe6e9e02750c9d8364a7b4439
Florvin Bayaborda <61019061+Florvin-04@users.noreply.github.com>
2023-04-08 23:56:16 +0800

    challenge done by frontendmentor.io

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,65 @@ import ThankYouPage from "./form-pages/thankYouPage";
 
 export const FormContext = createContext(null);
 
+const initialData = {
+  name: "",
+  email: "",
+  phone: "",
+  selectedPlan: {
+    id: 1,
+    billingType: "monthly",
+    name: "Arcade",
+    price: "9",
+  },
+  addOns: [],
+};
+
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [toggleBiling, setToggleBilling] = useState("monthly");
+  const [submitting, setSubmitting] = useState(false);
+  const [errors, setErrors] = useState({});
 
   const [formData, setFormData] = useState(
-    JSON.parse(localStorage.getItem("formData")) || {
-      name: "",
-      email: "",
-      phone: "",
-      selectedPlan: {
-        id: 1,
-        billingType: "monthly",
-        name: "Arcade",
-        price: "9",
-      },
-      addOns: [],
-    }
+    JSON.parse(localStorage.getItem("formData")) || initialData
   );
 
   useEffect(() => {
     localStorage.setItem("formData", JSON.stringify(formData));
-    console.log(formData);
+    // console.log(formData);
   }, [formData]);
+
+  function validateFields(formData) {
+    const errors = {};
+
+    if (formData.name === "") {
+      errors.name = "This field is required";
+    }
+    if (formData.email === "") {
+      errors.email = "This field is required";
+    }
+    if (formData.phone === "") {
+      errors.phone = "This field is required";
+    }
+    return errors;
+  }
+
+  function nextPage() {
+    console.log(errors);
+    console.log(Object.keys(errors).length, submitting);
+    if (Object.keys(errors).length === 0 && submitting) {
+      setCurrentPage((prev) => prev + 1);
+      if (currentPage === 4) {
+        alert("thanks");
+        setFormData(initialData);
+      }
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
+    setErrors(validateFields(formData));
+    setSubmitting(true);
   }
 
   return (
@@ -51,7 +84,14 @@ function App() {
             onSubmit={handleSubmit}
           >
             <FormContext.Provider
-              value={{ formData, setFormData, toggleBiling, setToggleBilling }}
+              value={{
+                formData,
+                setFormData,
+                toggleBiling,
+                setToggleBilling,
+                setCurrentPage,
+                errors,
+              }}
             >
               {currentPage === 1 && <Page1 />}
               {currentPage === 2 && <Page2 />}
@@ -71,10 +111,10 @@ function App() {
             </button>
 
             <button
-              type={currentPage === 4 ? "submit" : "button"}
+              type="submit"
               className="next-btn"
               form="form"
-              onClick={() => setCurrentPage((prev) => prev + 1)}
+              onClick={nextPage}
             >
               {currentPage === 4 ? "Confirm" : "Next Step"}
             </button>
